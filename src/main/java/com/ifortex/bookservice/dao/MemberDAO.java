7846dd8aa8dede3607634a5f8211363a3a1e5d59
neok <xpor2002@gmail.com>
2025-01-10 19:03:10 +0300

    minor

--- src/main/java/com/ifortex/bookservice/dao/MemberDAO.java
+++ src/main/java/com/ifortex/bookservice/dao/MemberDAO.java
@@ -4,6 +4,7 @@ import com.ifortex.bookservice.model.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,12 +13,14 @@ import java.util.List;
 public class MemberDAO {
     @PersistenceContext
     private EntityManager entityManager;
+    @Transactional(readOnly = true)
     public List<Member> findByBorrowedBooksEmptyAndMembershipDateYear(LocalDateTime membershipDate){
         return  entityManager.createQuery("select m from Member m " +
                 "WHERE m.borrowedBooks IS EMPTY and year(m.membershipDate)=?1")
                 .setParameter(1,membershipDate.getYear())
                 .getResultList();
     }
+    @Transactional(readOnly = true)
     public Member findByOldestGenreBookOrderByMembershipDate(String genre){
         return (Member) entityManager.createNativeQuery(
                      "SELECT * " +
